19f288d6748195c65053ee171bcdb9f74c499682
SmoIIy <91464612+SmoIIy@users.noreply.github.com>
2023-09-11 14:34:12 +0200

    Supabase test fix

--- supabasetest/script.js
+++ supabasetest/script.js
@@ -1,24 +1,25 @@
-const url = "https://hnrcndxoquhelmpaqheh.supabase.co";
+const url = "https://hnrcndxoquhelmpaqheh.supabase.co/rest/v1/Dataset";
 const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhucmNuZHhvcXVoZWxtcGFxaGVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4MjI5MTksImV4cCI6MjAwOTM5ODkxOX0.Kj0vgWH3imfxSBjrhKN9746mppt3v9ldhd1y2ugyfyc";
 
-fetch(url+"/rest/v1/Dataset",{
+fetch(url,{
     method: "GET",
     headers: {
     apikey: apikey
     }
 })
     .then(res=>res.json())
-    .then(showData)
+    .then(showData);
 
 
 function showData(items) {
+    console.log(items);
     items.forEach(showItem);
     
 }
 
 
 function showItem(item){
-    console.log(item);
+    console.log("item");
     const template = document.querySelector("template").content;
     const copy = template.cloneNode(true);
     copy.querySelector(".column-text-title").textContent = "Navn: " + item.title;
